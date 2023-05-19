1605784593cfb5aaf69e2d755a412b0b0fb6025d
AndyLu <laserlue@gmail.com>
2023-05-19 09:42:58 +0800

    docs: update README.md
    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # R-regex
 
 Building regex from scratch in Rust.
+
+## Reference
+
+- [Regex Library in Rust from Scratch (Finite-State Machines)](https://www.youtube.com/watch?v=MH56D5M9xSQ&t=2215s&ab_channel=TsodingDaily)
--- main.rs
+++ main.rs
@@ -1,3 +1,19 @@
+type FsmIndex = usize;
+
+struct FsmColumn {
+    ts: [FsmIndex; 127],
+}
+
+impl FsmColumn {
+    fn new() -> Self {
+        Self { ts: [0; 127] }
+    }
+
+    fn fill_range() {
+        todo!();
+    }
+}
+
 fn main() {
     println!("Hello")
 }
